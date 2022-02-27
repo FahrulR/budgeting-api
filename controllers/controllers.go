78249cacba7fb292b7104e913d85626e7a4dee8a
fahrul.rozi <fahrulrozi1288@gmail.com>
2022-02-27 10:08:27 +0700

    Adjust forgot password url

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -263,7 +263,7 @@ func sendEmailReset(email, token string) error {
 		return err
 	}
 
-	url := os.Getenv("WEB_URL") + "?token=" + token
+	url := os.Getenv("WEB_URL") + "/forgot-password?token=" + token
 
 	content := strings.ReplaceAll(string(body), "%URL%", url)
 
